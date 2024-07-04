aeb653079bd04ff02bea384779dd10935ed47a79
shokuyansh <147203868+shokuyansh@users.noreply.github.com>
2024-07-04 16:28:28 +0530

    Add files via upload

---
+++ 231A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        int p,v,t;
+        cin>>p>>v>>t;
+        if(p==1&&v==1||p==1&&t==1||v==1&&t==1)
+            count++;
+
+    }
+    cout<<count;
+}
